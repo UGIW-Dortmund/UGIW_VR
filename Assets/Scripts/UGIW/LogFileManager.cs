2d5e4482626dd6f315915e45c4acb6938946bc83
Enrico Guerra <enrico.guerra002@stud.fh-dortmund.de>
2021-07-30 12:05:33 +0200

    minor adjustments

--- Assets/Scripts/UGIW/LogFileManager.cs
+++ Assets/Scripts/UGIW/LogFileManager.cs
@@ -34,33 +34,33 @@ public class LogFileManager : MonoBehaviour
     {
         _dto = DateTimeOffset.Now;
         _concatenatedLogString += "| UGIW | General_msg | " + message + " |"
-            + _dto.LocalDateTime.ToString("dd/MM/yyyy hh:mm:ss.fff tt") + " |" + "\n";
+            + _dto.LocalDateTime.ToString("dd/MM/yyyy hh:mm:ss.fff tt") + "" + "\n";
     }
     public void PickedUp(GameObject controller)
     {
         _dto = DateTimeOffset.Now;
         _concatenatedLogString += "| UGIW | Object picked up with " + controller.name + " | "  
-            + _dto.LocalDateTime.ToString("dd/MM/yyyy hh:mm:ss.fff tt") + " |" + "\n";
+            + _dto.LocalDateTime.ToString("dd/MM/yyyy hh:mm:ss.fff tt") + "" + "\n";
     }
 
     public void Released(GameObject controller)
     {
         _dto = DateTimeOffset.Now;
         _concatenatedLogString += "| UGIW | Object dropped with " + controller.name + " | "
-            + _dto.LocalDateTime.ToString("dd/MM/yyyy hh:mm:ss.fff tt") + " |" + "\n";
+            + _dto.LocalDateTime.ToString("dd/MM/yyyy hh:mm:ss.fff tt") + "" + "\n";
     }
 
     public void StartHoveringInteractable(GameObject controller)
     {
         _dto = DateTimeOffset.Now;
         _concatenatedLogString += "| UGIW | Starting to hover over interactable with " + controller.name + " | "
-            + _dto.LocalDateTime.ToString("dd/MM/yyyy hh:mm:ss.fff tt") + " |" + "\n";
+            + _dto.LocalDateTime.ToString("dd/MM/yyyy hh:mm:ss.fff tt") + "" + "\n";
     }
     public void EndHoveringInteractable(GameObject controller)
     {
         _dto = DateTimeOffset.Now;
         _concatenatedLogString += "| UGIW | Stops to hover over interactable with " + controller.name + " | "
-            + _dto.LocalDateTime.ToString("dd/MM/yyyy hh:mm:ss.fff tt") + " |" + "\n";
+            + _dto.LocalDateTime.ToString("dd/MM/yyyy hh:mm:ss.fff tt") + "" + "\n";
     }
 
     public void SaveLogFile()
